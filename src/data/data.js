a2e0f1c5c26199760f28b23c266c3c33aadb1026
ahmedouvadel <ahmedvadel94@gmail.com>
2024-04-24 01:08:44 +0200

    first commit

---
+++ src/data/data.js
@@ -0,0 +1,56 @@
+import project1 from "../img/project1.png";
+import project2 from "../img/project1.png";
+import project3 from "../img/project1.png";
+import project4 from "../img/project1.png";
+import project5 from "../img/project1.png";
+
+export const HERO_CONTENT = `I am a dedicated student software engineer with a strong passion for building scalable and efficient applications. My academic background and hands-on experience have equipped me with profound knowledge and skills in a wide array of technologies and libraries, including Angular, Spring Boot, React, React Native, Android, .NET Core, HTML, CSS, Tailwind CSS, Material-UI, and Bootstrap, as well as databases like MySQL and MongoDB.`;
+
+export const ABOUT_TEXT = `My approach combines best practices with cutting-edge technologies to deliver exceptional results. I thrive in dynamic environments and am adept at managing multiple projects simultaneously, all while ensuring meticulous attention to detail and adherence to deadlines. Whether you need a complex enterprise application or a responsive design, I am here to help you navigate the technical challenges and create a product that aligns perfectly with your vision.
+
+I look forward to collaborating on innovative projects and turning your ideas into reality!`;
+
+
+export const PROJECTS = [
+  {
+    title: "E-Commerce mobile",
+    image: project1,
+    description:
+      "A fully functional e-commerce mobile with features like product listing, shopping cart, and user authentication.",
+    technologies: ["react-native", "springboot", "MySql", "Postman", "Expo"],
+  },
+  {
+    title: "E-Commerce Web",
+    image: project2,
+    description:
+    "A fully functional e-commerce web with features like Add product, Add Categorie , Tracking the Order , and admin, Agent authentication.",
+    technologies: ["Material UI", "Bootstrap", "Angular", "Springboot", "MySql", "Postman" ],
+  },
+  {
+    title: "Portfolio Website",
+    image: project3,
+    description:
+      "A personal portfolio website showcasing projects, skills, and contact information.",
+    technologies: ["HTML", "CSS", "React", "tailwind css"],
+  },
+  {
+    title: "E-Commerce Website",
+    image: project4,
+    description:
+    "A fully functional e-commerce website with features like product listing, product details , shopping cart, and user authentication.",
+    technologies: ["HTML", "CSS", "Bootstrap", "dotNet Core Mvc", "Swagger", "mySQL"],
+  },
+  {
+    title: "E-Commerce mobile",
+    image: project5,
+    description:
+    "A fully functional e-commerce website with features like product listing, product details , Add product ,  and user authentication.",
+    technologies: ["Java", "Android", "Firebase"],
+  },
+];
+
+export const CONTACT = {
+  address: "Sousse, Sahloul",
+  phoneNo: "00216 54769409 ",
+  email: "ahmedvadel94@gmail.com",
+};
