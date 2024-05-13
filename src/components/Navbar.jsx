d177f89db841c4d4f92c0c3dda12a9208a155edd
ahmedouvadel <ahmedvadel94@gmail.com>
2024-05-13 16:57:25 +0200

    bro

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,13 +6,13 @@ import { FaInstagram } from "react-icons/fa";
 const Navbar = () => {
   return <nav className="mb-20 flex items-center justify-between py-6">
     <div className="flex flex-shrink-0 items-center">
-      <img className="mx-2 w-20 " src={logo} alt="logo"  />
+      <img className="mx-2 w-24 " src={logo} alt="logo"  />
     </div>
     <div className="m-8 flex items-center justify-center gap-4 text-2xl">
-      <a href=""><FaLinkedin  /></a>
+      <a href="https://www.linkedin.com/in/ahmedou-vadel-346bb6231/"><FaLinkedin  /></a>
       <a href="https://github.com/ahmedouvadel"><FaGithub /></a>
-      <a href=""><FaFacebook /></a>
-      <a href=""><FaInstagram /></a>
+      <a href="https://www.facebook.com/ahmed.vadel.79/"><FaFacebook /></a>
+      <a href="https://www.instagram.com/_te_mo___/"><FaInstagram /></a>
       
     </div>
   </nav>
